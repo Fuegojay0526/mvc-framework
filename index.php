30f459bdf53bd94b5aa9b27d48fee3e0948f4910
Fuegojay0526 <nonaybanan@gmail.com>
2022-08-04 11:05:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,11 @@
 $Host = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
 //dirname($_SERVER['PHP_SELF']) show your directory where the file located
 $URL = $Host . dirname($_SERVER['PHP_SELF']);
+//explode the "/" in your directory
+$CurrentDIR = explode("/", dirname($_SERVER['PHP_SELF']));
+$CurrentDIR = $CurrentDIR[count($CurrentDIR)-1];
+//explode the "/" in your parameter example folder
+$Parameter = explode($CurrentDIR."/", dirname($_SERVER['REQUEST_URI']));
+$Parameter = strtolower($Parameter[count($Parameter)-1]);
 
 ?>
